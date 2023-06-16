bf21c87926655abf82576b1ff17fdb38cd9dc01d
seung <ooo167@naver.com>
2023-06-17 01:06:48 +0900

    login 기능추가
    firebase auth 사용 login 구현

---
+++ Assets/scripts/AuthManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.UI;
+
+public class AuthManager : MonoBehaviour
+{
+    [SerializeField] InputField emailField;
+    [SerializeField] InputField pwField;
+
+    // 인증을 관리할 객체 선언
+    Firebase.Auth.FirebaseAuth auth;
+
+    void Awake()
+    {
+        // 객체 초기화 
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    }
+    public void loginUserBtn()
+    {
+        // 제공되는 함수 : email 과 pw로 로그인
+        auth.SignInWithEmailAndPasswordAsync(emailField.text, pwField.text).ContinueWith(
+            task => {
+                if (task.IsCompleted && !task.IsFaulted && !task.IsCanceled)
+                {
+                    Debug.Log(emailField.text + "님 환영합니다!");
+                }
+                else 
+                {
+                    Debug.Log("아이디와 비밀번호를 확인해주세요.");
+                }
+            }
+        );
+    }
+    public void addUserBtn() {
+        // 제공 되는 함수 : 이메일과 비번으로 회원가입
+        auth.CreateUserWithEmailAndPasswordAsync(emailField.text, pwField.text).ContinueWith(
+            task => {
+                if (!task.IsCanceled && !task.IsFaulted) 
+                {
+                    Debug.Log(emailField.text + "로 회원가입 완료");
+                }
+                else
+                {
+                    Debug.Log("회원가입 실패");
+                }
+            }
+        );
+    }
+}
