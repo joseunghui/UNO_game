a58ad8cda0307356bdd244edeeedf8f5896cae1f
seung <ooo167@naver.com>
2023-06-20 00:10:50 +0900

    main image add
    우노 메인이미지 넣고 로그인용 팝업 생성

---
+++ Assets/scripts/MainLoad.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MainLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Init();
+        transform.localScale = Vector3.one * 0.1f;
+        gameObject.SetActive(false);
+    }
+
+    public void Show() 
+    {
+        gameObject.SetActive(true);
+        var seq = DOTween.Sequence();
+
+        seq.Append(transform.DOScale(1.1f, 0.2f));
+        seq.Append(transform.DOScale(1f, 0.1f));
+
+        seq.Play();
+    }
+
+    public void Hide()
+    {
+        var seq = DOTween.Sequence();
+
+        transform.localScale = Vector3.one * 0.2f;
+
+        seq.Append(transform.DOScale(1.1f, 0.1f));
+        seq.Append(transform.DOScale(0.2f, 0.2f));
+
+        seq.Play().OnComplete(() => 
+        {
+            gameObject.SetActive(false);
+        });
+    }
+}
