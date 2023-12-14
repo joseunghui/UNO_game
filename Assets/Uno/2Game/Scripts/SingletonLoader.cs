05138143174d5e7a1e7b37fee7dd7f7c9f3e0d1b
seung00097@gmail.com <88180714+joseunghui@users.noreply.github.com>
2023-12-14 09:12:58 +0900

    중간저장4(컨트롤러 분리)

--- Assets/Uno/2Game/Scripts/SingletonLoader.cs
+++ Assets/Uno/2Game/Scripts/SingletonLoader.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 public class SingletonLoader : MonoBehaviour
 {
     public GameObject entityManager;
-    public GameObject gameManager;
     public GameObject turnManager;
     public GameObject cardManager;
 
     void Awake()
     {
-        GameManager.Load(gameManager);
         EntityManager.Load(entityManager);
         TurnManager.Load(turnManager);
         CardManager.Load(cardManager);
