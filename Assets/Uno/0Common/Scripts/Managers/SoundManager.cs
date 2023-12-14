a8b6434451f88b46fc88dca2c6bd082e1bfe999c
seung <88180714+joseunghui@users.noreply.github.com>
2023-12-14 14:20:10 +0900

    SoundManager

--- Assets/Uno/0Common/Scripts/Managers/SoundManager.cs
+++ Assets/Uno/0Common/Scripts/Managers/SoundManager.cs
@@ -57,23 +57,6 @@ public class SoundManager
         Play(audioClip, type, pitch);
     }
 
-    /*
-    public void SetBgmVolume(float volume){
-    AudioSource bgmSource = GameManager.instance.GetComponent<AudioSource>();
-    bgmSource.volume = volume;
-}
-public void SetEffectVolume(float volume){
-    AudioSource effectSource = CardManager.instance.GetComponent<AudioSource>();
-    effectSource.volume = volume;
-}
-public void CannotClick(){
-    TurnManager.instance.isLoading = true;
-}
-public void CanClick(){
-    TurnManager.instance.isLoading = false;
-}
- */
-
     // 오디오 클립을 직접 받는 Play() 버전
     public void Play(AudioClip audioClip, Define.Sound type = Define.Sound.Effect, float pitch = 1.0f) // 기본은 Effect
     {
