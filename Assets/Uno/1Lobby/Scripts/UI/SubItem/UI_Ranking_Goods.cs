0ac47038bdcc5b079b609289498ce7cd48d2848b
seung <88180714+joseunghui@users.noreply.github.com>
2024-01-03 13:30:23 +0900

    하트 다이아 연결

--- Assets/Uno/1Lobby/Scripts/UI/SubItem/UI_Ranking_Goods.cs
+++ Assets/Uno/1Lobby/Scripts/UI/SubItem/UI_Ranking_Goods.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class UI_Ranking_Goods : UI_SubItem
 {
+    public Sprite HeartImage;
+    public Sprite DiaImage;
+
     public Define.Goods _type = Define.Goods.Heart; // basic = heart
     private UserInfoData tempUserInfo;
 
@@ -26,10 +29,12 @@ public class UI_Ranking_Goods : UI_SubItem
         switch(_type)
         {
             case Define.Goods.Heart:
+                GetImage((int)Define.Images.GoodsIconImage).gameObject.GetComponent<Image>().sprite = HeartImage;
+                GetText((int)Define.Texts.GoodsText).gameObject.GetComponent<TextMeshProUGUI>().text = tempUserInfo.heart.ToString();
                 break;
             case Define.Goods.Dia:
-                GetImage((int)Define.Images.GoodsIconImage).gameObject.GetComponent<Image>().sprite = null;
-                GetText((int)Define.Texts.GoodsText).gameObject.GetComponent<TextMeshProUGUI>().text = null;
+                GetImage((int)Define.Images.GoodsIconImage).gameObject.GetComponent<Image>().sprite = DiaImage;
+                GetText((int)Define.Texts.GoodsText).gameObject.GetComponent<TextMeshProUGUI>().text = (tempUserInfo.freeDia + tempUserInfo.payDia).ToString();
                 break;
         }
 
